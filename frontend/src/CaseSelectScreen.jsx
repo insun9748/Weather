472cb6d7eb2a31dd8d69b2e7626792cc2dc267ba
insun9748 <insun9748@gmail.com>
2026-07-30 18:29:30 +0900

    Use 2-star rating for the 2018 case and update its conclusion ribbon art

--- frontend/src/CaseSelectScreen.jsx
+++ frontend/src/CaseSelectScreen.jsx
@@ -2,6 +2,7 @@ import file2020 from './assets/2020_file.png'
 import file2018 from './assets/2018_file.png'
 import file2022 from './assets/2022_file.png'
 import background from './assets/file_bg.png'
+import star2 from './assets/star2.png'
 import star3 from './assets/star3.png'
 import './Stage.css'
 import './CaseSelectScreen.css'
@@ -10,7 +11,7 @@ const STAR_COLORS = ['#368F39', '#DB9D00', '#6F839F', '#368F39', '#DB9D00']
 
 const CASES = [
   { id: '2020', image: file2020, alt: '2020 역대 최장 장마 사건', starImage: star3 },
-  { id: '2018', image: file2018, alt: '2018 기록적 폭염 사건' },
+  { id: '2018', image: file2018, alt: '2018 기록적 폭염 사건', starImage: star2 },
   { id: '2022', image: file2022, alt: '2022 수도권 집중호우 사건' },
 ]
 
